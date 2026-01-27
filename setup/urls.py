232db229e24851d1670c8510a10a03ff7d338f06
Murilo <murilobarros304@gmail.com>
2026-01-27 16:57:46 -0300

    feat: redefinição de senha funcionando no DEBUG

--- setup/urls.py
+++ setup/urls.py
@@ -28,4 +28,9 @@ urlpatterns = [
     path('login', CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('signup', SignUpView.as_view(), name='signup'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
