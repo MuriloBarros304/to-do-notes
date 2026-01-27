877e80160415bb3e7563bb917eb2b6c6e4509c37
Murilo <murilobarros304@gmail.com>
2026-01-27 17:26:18 -0300

    fix: Erro na porta do serviço de email

--- setup/settings.py
+++ setup/settings.py
@@ -158,7 +158,8 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = config('EMAIL_HOST')
     EMAIL_PORT = config('EMAIL_PORT', cast=int)
-    EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+    EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
+    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
     EMAIL_HOST_USER = config('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = 'To Do Notes <noreply@todonotes.com>'
